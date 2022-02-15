812dc74ed7a1f0d22eba140d72ed83a2e8a07436
bksarkar2507 <bishalkumarsarkar@gmail.com>
2022-02-15 23:21:10 +0600

    number button added

--- pin-match.js
+++ pin-match.js
@@ -13,6 +13,23 @@ document.getElementById('generate-button').addEventListener('click', function(){
     fourDigitPin();
 });
 
-document.getElementById('key-pad').addEventListener('click', function(){
-    console.log('key pad is clicked');
+document.getElementById('key-pad').addEventListener('click', function(event){
+    const typedNumber = event.target.innerText;
+    const previousNumber = document.getElementById('typed-pin');
+
+    if(isNaN(typedNumber)){
+        if(typedNumber == 'C'){
+            previousNumber.value = '';
+        }
+    }
+    else{
+        const previousNumberValue = previousNumber.value;
+
+        const newNumber = previousNumberValue + typedNumber;
+        previousNumber.value = newNumber;
+    }
+});
+
+document.getElementById('submit-button').addEventListener('click', function(){
+    console.log('submit button is clicked');
 });
