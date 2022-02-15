668fbfb80f17a84f5e945f04a9f75ed1ddb998e4
bksarkar2507 <bishalkumarsarkar@gmail.com>
2022-02-15 15:51:06 +0600

    3:50 pm

--- pin-match.js
+++ pin-match.js
@@ -1,9 +1,18 @@
 function fourDigitPin(){
     let i = Math.round(Math.random() * 10000);
-    console.log(typeof i);
+    const displayPin = document.getElementById('displayGeneratePin');
+    if(i > 1000){
+        displayPin.value = i;
+    }
+    else{
+        fourDigitPin();
+    }
 }
 
 document.getElementById('generate-button').addEventListener('click', function(){
-    const displayPin = document.getElementById('displayGeneratePin');
     fourDigitPin();
+});
+
+document.getElementById('key-pad').addEventListener('click', function(){
+    console.log('key pad is clicked');
 });
